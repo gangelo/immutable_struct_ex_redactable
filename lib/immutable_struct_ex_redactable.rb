4658e00719eef1ab042fe774909ca1e2d229ed78
gangelo <web.gma@gmail.com>
2022-10-01 17:34:56 -0400

    Initial commit

---
+++ lib/immutable_struct_ex_redactable.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "immutable_struct_ex_redactable/version"
+
+module ImmutableStructExRedactable
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/immutable_struct_ex_redactable/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ImmutableStructExRedactable
+  VERSION = "0.1.0"
+end
