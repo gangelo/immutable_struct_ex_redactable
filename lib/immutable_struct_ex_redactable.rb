a4cb5af9058581afc95650aec6b72d84ddf8a0bd
gangelo <web.gma@gmail.com>
2022-10-01 17:53:12 -0400

    Fix rubocop violations

--- lib/immutable_struct_ex_redactable.rb
+++ lib/immutable_struct_ex_redactable.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "immutable_struct_ex_redactable/version"
+require_relative 'immutable_struct_ex_redactable/version'
 
 module ImmutableStructExRedactable
   class Error < StandardError; end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,11 +7,11 @@ SimpleCov.start do
   add_filter 'spec'
 end
 
-require "immutable_struct_ex_redactable"
+require 'immutable_struct_ex_redactable'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
