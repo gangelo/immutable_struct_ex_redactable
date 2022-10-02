2143b43f802088f7342caa42c01ecee72e1b503d
gangelo <web.gma@gmail.com>
2022-10-02 16:03:03 -0400

    Create the gem main module funcs

--- lib/immutable_struct_ex_redactable.rb
+++ lib/immutable_struct_ex_redactable.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
+require 'immutable_struct_ex'
+require_relative 'immutable_struct_ex_redactable/configuration'
 require_relative 'immutable_struct_ex_redactable/version'
 
 module ImmutableStructExRedactable
-  class Error < StandardError; end
-  # Your code goes here...
+  module_function
+
+  def create(**hash, &block)
+    config = ImmutableStructExRedactable.configure
+    create_with(config, **hash, &block)
+  end
+
+  def create_with(config, **hash, &block)
+    config.redacted.each do |attr|
+      next unless hash.key? attr
+
+      hash[attr] = config.redacted_label
+    end
+
+    ImmutableStructEx.new(**hash, &block)
+  end
 end
